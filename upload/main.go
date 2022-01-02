a9f2259c46e73120aec38051a835f106d409d77d
aherrera <aherrera@twilio.com>
2022-01-01 22:25:58 -0500

    Deployment: Se remplaza la acción de subir a S3 por el awscli

--- upload/main.go
+++ upload/main.go
@@ -17,7 +17,7 @@ import (
 
 	"github.com/google/uuid"
 
-	// TODO: Must be removed this external libray ?
+	// TODO: Must be removed this external libray?
 	"github.com/mitchellh/mapstructure"
 )
 
